f51f0d825e38455dddfb3712ff8b4ac5ba2517fc
Tim Cecatka <tim.cecatka@gmail.com>
2020-11-11 15:36:23 +0100

    Restaurant Controllers and Views ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
-  resources :restaurants do
+  resources :restaurants, only: [ :index, :show, :new, :create, ] do
     resources :reviews, only: [ :new, :create ]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'clearing Database'
+Restaurant.destroy_all
+
+puts 'Creating 5 Restaurant seeds'
+
+Restaurant.create!(name: 'Restaurant Numero 1', address: 'address1', phone_number: '12345', category: 'chinese')
+Restaurant.create!(name: 'Restaurant Numero 2', address: 'address2', phone_number: '12346', category: 'italian')
+Restaurant.create!(name: 'Restaurant Numero 3', address: 'address3', phone_number: '12347', category: 'japanese')
+Restaurant.create!(name: 'Restaurant Numero 4', address: 'address4', phone_number: '12348', category: 'french')
+Restaurant.create!(name: 'Restaurant Numero 5', address: 'address5', phone_number: '12349', category: 'belgian')
+
+puts 'finished'
